e6b403bc76613a46cbf1cb1352e9f43036e9c0f6
usk <22q221q@gmail.com>
2023-03-01 11:31:19 +0900

    lintエラー修正

--- src/presentation/libtest/index.tsx
+++ src/presentation/libtest/index.tsx
@@ -1,6 +1,7 @@
+import { ReactNode } from "react";
 import { ThemeProvider } from "@chakra-ui/react";
 import theme from "presentation/theme";
 
-export const UITestWrapper = ({ children }) => (
+export const UITestWrapper = ({ children }: { children: ReactNode }) => (
   <ThemeProvider theme={theme}>{children}</ThemeProvider>
 );
